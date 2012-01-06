fe227cb47c8590228eff9b00a1116eee587935f4
Christoph Viebig <christoph.viebig@gmx.net>
2012-01-06 10:36:42 +0100

    Added license header

--- src/com/github/jgility/e4/Activator.java
+++ src/com/github/jgility/e4/Activator.java
@@ -1,3 +1,16 @@
+/*
+ * 
+ * Copyright (c) 2011 by Jgility Development Group
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Christoph Viebig
+ *
+ */
+
 package com.github.jgility.e4;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
